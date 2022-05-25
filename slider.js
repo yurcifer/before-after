0151488d6b39040241cdc1b65c06a06233a9a2a0
Yurii Sh <yurcifer.dev@gmail.com>
2022-05-25 18:30:03 +0300

    added css classes. Added ability to move slider

---
+++ slider.js
@@ -0,0 +1,61 @@
+function getTemplate(state) {
+    return `
+    <div class="slider__before" style="width: ${state.width}px; background-image: url(${state.before})" >
+        <div class="slider__resize" data-type="resize"></div>
+    </div>
+    <div class="slider__after" style="background-image: url(${state.after})"></div>
+`
+}
+class Slider {
+    constructor (selector, state) {
+        this.$slider = document.getElementById(selector);
+        this.state = {
+            ...state,
+            width: state.width || 512
+        };
+        this.#render(this.state);
+        this.#listen();
+    }
+
+    #render (state) {
+        this.$slider.innerHTML = getTemplate(state);
+    }
+
+    #update (props) {
+        this.state = {
+            ...this.state,
+            ...props
+        }
+        this.#render(this.state);
+    }
+
+    #listen () {
+        this.mouseDownHandler = this.mouseDownHandler.bind(this)
+        this.mouseUpHandler = this.mouseUpHandler.bind(this)
+        this.mouseMoveHandler = this.mouseMoveHandler.bind(this)
+        this.$slider.addEventListener('mousedown', this.mouseDownHandler)
+        this.$slider.addEventListener('mouseup', this.mouseUpHandler)
+    }
+
+    mouseDownHandler (event) {
+        if (event.target.dataset.type === 'resize')
+        this.$slider.addEventListener('mousemove', this.mouseMoveHandler);
+        this.currentPosition = event.clientX;
+        console.log(this.currentPosition);
+    }
+
+    mouseUpHandler () {
+        this.$slider.removeEventListener('mousemove', this.mouseMoveHandler);
+    }
+
+    mouseMoveHandler (event) {
+        let shift = this.currentPosition - event.clientX;
+        this.currentPosition = event.clientX;
+        this.#update({width: this.state.width- shift});
+    }
+}
+
+const slider = new Slider('slider', {
+    before: './assets/before.png',
+    after: './assets/after.png',
+});
